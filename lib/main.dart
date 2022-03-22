4e2a490a2f6eeed97691ae256889910d0e549168
KYUNG KOOK SON <kyungkook.son@gmail.com>
2022-03-22 22:20:24 +0900

    fix: reverse to view screens

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // initialRoute: '/', // 초기 메인 화면
-      // routes: {  // 그 외 라우트들 목록
-      //   '/' : (_) => ScreenA(),
-      //   '/b' : (_) => ScreenB(),
-      //   '/c' : (_) => ScreenC(),
-      // },
-      home: drawer(), // drawer가 있는 화면으로 간다.
+      initialRoute: '/', // 초기 메인 화면
+      routes: {  // 그 외 라우트들 목록
+        '/' : (_) => ScreenA(),
+        '/b' : (_) => ScreenB(),
+        '/c' : (_) => ScreenC(),
+      },
+      // home: drawer(), // drawer가 있는 화면으로 간다.
     );
   }
 }
