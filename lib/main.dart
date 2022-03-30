ed8a85a55c16f0661c94262d1a24856037eb3429
Kyung Kook Son <kyungkook.son@gmail.com>
2022-03-30 20:35:56 +0900

    feat: add buttons that plus or minus 1

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,70 @@ import 'package:flutter_app_icon_menu/drawer.dart';
 
 void main() => runApp(MyApp()); // Everything starts here.
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  int count = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/', // 초기 메인 화면
-      routes: {  // 그 외 라우트들 목록
-        '/' : (_) => ScreenA(),
-        '/b' : (_) => ScreenB(),
-        '/c' : (_) => ScreenC(),
-      },
-      // home: drawer(), // drawer가 있는 화면으로 간다.
+      theme: ThemeData(
+        primaryColor: Colors.amber,
+      ),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("You have pushed the button this many times"),
+              Text(
+                "$count",
+                style: Theme.of(context).textTheme.displaySmall,
+              ),
+              // Checkbox(
+              //     value: true,
+              //     onChanged: (isChecked) {
+              //       print("isChecked: $isChecked");
+              //     },
+              // ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  FloatingActionButton(
+                    child: Icon(Icons.add),
+                    onPressed: () {
+                      setState(() {
+                        count++;
+                        print("counting: $count");
+                      });
+                    },
+                  ),
+                  SizedBox(width: 20,),
+                  FloatingActionButton(
+                    child: Icon(Icons.remove),
+                    onPressed: () {
+                      setState(() {
+                        count--;
+                        print("counting: $count");
+                      });
+                    },
+                  ),
+                ],
+              ),
+
+            ],
+          ),
+        ),
+
+      )
     );
   }
 }
